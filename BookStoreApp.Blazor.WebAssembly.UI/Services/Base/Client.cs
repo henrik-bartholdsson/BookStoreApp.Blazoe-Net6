3a8cacb4e26e2f149af34f5bf46edf49a8c4d900
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2022-09-21 18:44:43 +0200

    Adding webassembly ui project.

---
+++ BookStoreApp.Blazor.WebAssembly.UI/Services/Base/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreApp.Blazor.WebAssembly.UI.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get { return _httpClient; }
+        }
+    }
+}
---
+++ BookStoreApp.Blazor.WebAssembly.UI/Services/BookService.cs
@@ -0,0 +1,145 @@
+﻿using AutoMapper;
+using Blazored.LocalStorage;
+using BookStoreApp.Blazor.WebAssembly.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.WebAssembly.UI.Services
+{
+    public class BookService : BaseHttpService, IBookService
+    {
+        private readonly IClient _client;
+        private readonly IMapper mapper;
+
+        public BookService(IClient client, ILocalStorageService localStorage, IMapper mapper)
+            : base(client, localStorage)
+        {
+            _client = client;
+            this.mapper = mapper;
+        }
+
+        public async Task<Response<int>> Create(BookCreateDto book)
+        {
+            Response<int> response = new Response<int> { };
+            try
+            {
+                await GetBearerToken();
+                await _client.BooksPOSTAsync(book);
+            }
+            catch (ApiException exception)
+            {
+                response = ConvertApiExceptions<int>(exception);
+            }
+
+            return response;
+        }
+
+        public async Task<Response<int>> Delete(int id)
+        {
+            Response<int> response = new();
+
+            try
+            {
+                await GetBearerToken();
+                await _client.BooksDELETEAsync(id);
+            }
+            catch (ApiException ex)
+            {
+
+                response = ConvertApiExceptions<int>(ex);
+            }
+
+            return response;
+        }
+
+        public async Task<Response<int>> Edit(int id, BookUpdateDto book)
+        {
+            Response<int> response = new Response<int> { };
+            try
+            {
+                await GetBearerToken();
+                await _client.BooksPUTAsync(id, book);
+            }
+            catch (ApiException exception)
+            {
+                response = ConvertApiExceptions<int>(exception);
+            }
+
+            return response;
+        }
+
+        public async Task<Response<BookDetailsDto>> Get(int id)
+        {
+            Response<BookDetailsDto> response;
+
+            try
+            {
+                await GetBearerToken();
+
+                var data = await _client.BooksGETAsync(id);
+                response = new Response<BookDetailsDto>
+                {
+                    Data = data,
+                    Success = true
+                };
+            }
+            catch (ApiException exception)
+            {
+                response = ConvertApiExceptions<BookDetailsDto>(exception);
+            }
+
+
+            return response;
+        }
+
+        public async Task<Response<List<BookReadOnlyDto>>> Get()
+        {
+            Response<List<BookReadOnlyDto>> response;
+
+            try
+            {
+                await GetBearerToken();
+                var data = await _client.BooksAllAsync();
+                response = new Response<List<BookReadOnlyDto>>
+                {
+                    Data = data.ToList(),
+                    Success = true
+                };
+            }
+            catch (ApiException exception)
+            {
+                response = ConvertApiExceptions<List<BookReadOnlyDto>>(exception);
+            }
+
+
+            return response;
+        }
+
+        public async Task<Response<BookUpdateDto>> GetForUpdate(int id)
+        {
+            Response<BookUpdateDto> response;
+
+            try
+            {
+                await GetBearerToken();
+
+                var readOnlyDto = await _client.BooksGETAsync(id);
+
+                var data = mapper.Map<BookUpdateDto>(readOnlyDto);
+
+                data.Id = id;
+
+                response = new Response<BookUpdateDto>
+                {
+                    Data = data,
+                    Success = true
+                };
+            }
+            catch (ApiException exception)
+            {
+                response = ConvertApiExceptions<BookUpdateDto>(exception);
+            }
+
+
+            return response;
+        }
+    }
+}
